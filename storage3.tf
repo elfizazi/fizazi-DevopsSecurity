c842677ade8a015e7d988df12b5a0a4a07c880b7
Mohammed el Fizazi <126111695+elfizazi@users.noreply.github.com>
2026-03-28 16:37:49 +0000

    test direct main

---
+++ storage3.tf
@@ -0,0 +1,14 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "fizazistorage2"
+  resource_group_name      = rg-fizazi-storage2
+  location                 = westeurope
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  shared_access_key_enabled = true
+  public_network_access_enabled  = true
+  secret = welkom2022!
+
+  tags = {
+    environment = "staging"
+  }
+}
